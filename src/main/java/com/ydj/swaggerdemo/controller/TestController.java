b5a955cbb7c3737a06d17baaba817e846882c99c
yangdejun <martonyang0707@163.com>
2020-10-20 15:40:56 +0800

    update something

--- src/main/java/com/ydj/swaggerdemo/controller/TestController.java
+++ src/main/java/com/ydj/swaggerdemo/controller/TestController.java
@@ -2,6 +2,7 @@ package com.ydj.swaggerdemo.controller;
 
 import com.ydj.swaggerdemo.entity.User;
 import io.swagger.annotations.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +10,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/swagger")
 @Api(tags = {"swagger 注解使用示例"})
+@CrossOrigin
 public class TestController {
 
     @ApiOperation(
-            value = "根据用户 ID 和 用户名称获取用户"
+            value = "根据用户 ID 和 用户名称获取用户信息"
             , notes = "测试"
-            , httpMethod = "GET"
+            , httpMethod = "POST"
+            , produces = "application/json"
+            , protocols = "http"
+    )
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户 id 撒", required = true, paramType = "query", dataType = "int")
+            ,@ApiImplicitParam(name = "name", value = "用户名称撒", paramType = "query", dataType = "String")
+    })
+    @ApiResponse(
+            code = 200
+            , message = "request success ~~~"
+            , response = User.class
+    )
+    @GetMapping(value = "/test")
+    public User getUserByIdAndName (Integer id, String name) {
+        return new User(id, name);
+    }
+
+
+    @ApiOperation(
+            value = "根据用户 ID 称获取用户"
+            , notes = "测试"
+            , httpMethod = "POST"
             , produces = "application/json"
             , protocols = "http"
     )
@@ -27,8 +51,8 @@ public class TestController {
             , message = "request success ~~~"
             , response = User.class
     )
-    @GetMapping(value = "/test")
-    public User getUserByIdAndName (Integer id, String name) {
+    @GetMapping(value = "/haha")
+    public User getUserById (Integer id, String name) {
         return new User(id, name);
     }
 
